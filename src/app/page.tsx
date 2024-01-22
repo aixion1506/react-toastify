f43c72aa89f28971a3387dd168e429a20db44262
aixion <aixion1506@gmail.com>
2024-01-22 16:42:16 +0900

    component button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 'use client'
-import { ToastContainer, toast } from "react-toastify";
+import { Slide, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ErrorToast } from "./toast";
 
 export default function Home() {
-  const notify = () => {
-    toast.info("test toasting", {
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
-  };
+  // const notify = () => {
+  //   toast.info("test toasting", {
+  //     position: "top-center",
+  //     autoClose: 3000,
+  //     hideProgressBar: false,
+  //     closeOnClick: true,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //     theme: "light",
+  //   });
+  // };
+  const errorMessage = 'Login'
+  const notify = ErrorToast(errorMessage);
+  notify();
+
   return (
     <>
       <button onClick={notify}>notinoty</button>
--- src/app/toast.tsx
+++ src/app/toast.tsx
@@ -1,5 +1,17 @@
 import { toast } from 'react-toastify'
 
-export const ErrorToast = () => {
-    
+export const ErrorToast = (message : string) => {
+    return () => {
+        toast.success(message, {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            // transition: Bounce,
+        });
+    };
 }
